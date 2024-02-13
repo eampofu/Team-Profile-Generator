7bb1d9bac809903847cd72f5f5703a06d8c80c7a
Eddington <eampofu@gmail.com>
2024-02-13 23:59:08 +0200

    Added code to generate the team html file which also creates the output folder if not exists.

--- index.js
+++ index.js
@@ -10,137 +10,141 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-const team=[];
+const team = [];
 //prompt for the Manager
 const addManager = () => {
-    return inquirer.prompt ([
-        {
-            type: 'input',
-            name: 'name',
-            message: "What is the team manager's name?", 
-          
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: "What is the team manager's ID.",
-          
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: "What is the team manager's email.",
-         
-        },
-        {
-            type: 'input',
-            name: 'officeNumber',
-            message: "What is the team manager's office number",
-          
-        }
-    ])
-    .then(data => {
-        const  { name, id, email, officeNumber } = data; 
-        const manager = new Manager (name, id, email, officeNumber);
+	return inquirer
+		.prompt([
+			{
+				type: "input",
+				name: "name",
+				message: "What is the team manager's name?",
+			},
+			{
+				type: "input",
+				name: "id",
+				message: "What is the team manager's ID.",
+			},
+			{
+				type: "input",
+				name: "email",
+				message: "What is the team manager's email.",
+			},
+			{
+				type: "input",
+				name: "officeNumber",
+				message: "What is the team manager's office number",
+			},
+		])
+		.then((data) => {
+			const { name, id, email, officeNumber } = data;
+			const manager = new Manager(name, id, email, officeNumber);
 
-        team.push(manager); 
-        console.log(manager); 
-    })
+			team.push(manager);
+			// console.log(manager);
+		});
 };
 
 //prompt for the other members
-async function  addTeamMember()  {
-   
-    const askRole = await  inquirer.prompt([
-        {
-            type: 'list',
-            name: 'role',
-            message: "Please choose your employee's role",
-            choices: ['Engineer', 'Intern']
-        }
-    ]);
-        return inquirer.prompt ([
-            
-            {
-                type: 'input',
-                name: 'name',
-                message: `What is the name of the ${askRole.role}?`, 
-               
-            },
-            {
-                type: 'input',
-                name: 'id',
-                message: `Please enter the ${askRole.role}'s ID.`
-                
-                    
-                
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: `Please enter the ${askRole.role}'s email.`
-               
-            },
-            {
-                type: 'input',
-                name: 'github',
-                message: `Please enter the ${askRole.role}'s github username.`,
-                when: () => askRole.role === "Engineer"
-               
-            },
-            {
-                type: 'input',
-                name: 'school',
-                message: `Please enter the ${askRole.role}'s school`,
-                when: () => askRole.role === "Intern"
-              
-            },
-            {
-                type: 'confirm',
-                name: 'confirmAddNew',
-                message: 'Would you like to add more team members?',
-                default: false
-            }
-        ])
-        .then(data => {
-            // data for employee types 
-    
-            let { name, id, email,  github, school, confirmAddNew } = data; 
-            let employee; 
-    console.log(askRole.role)
-            if (askRole.role === "Engineer") {
-                employee = new Engineer (name, id, email, github);
-    
-                console.log(employee);
-    
-            } else if (askRole.role === "Intern") {
-                employee = new Intern (name, id, email, school);
-    
-                console.log(employee);
-            }
-    
-            team.push(employee); 
-    
-            if (confirmAddNew) {
-                return addTeamMember(team); 
-            } else {
-                return team;
-            }
-        })
-    
-    };
+async function addTeamMember() {
+	const askRole = await inquirer.prompt([
+		{
+			type: "list",
+			name: "role",
+			message: "Please choose your employee's role",
+			choices: ["Engineer", "Intern"],
+		},
+	]);
+	return inquirer
+		.prompt([
+			{
+				type: "input",
+				name: "name",
+				message: `What is the name of the ${askRole.role}?`,
+			},
+			{
+				type: "input",
+				name: "id",
+				message: `Please enter the ${askRole.role}'s ID.`,
+			},
+			{
+				type: "input",
+				name: "email",
+				message: `Please enter the ${askRole.role}'s email.`,
+			},
+			{
+				type: "input",
+				name: "github",
+				message: `Please enter the ${askRole.role}'s github username.`,
+				when: () => askRole.role === "Engineer",
+			},
+			{
+				type: "input",
+				name: "school",
+				message: `Please enter the ${askRole.role}'s school`,
+				when: () => askRole.role === "Intern",
+			},
+			{
+				type: "confirm",
+				name: "confirmAddNew",
+				message: "Would you like to add more team members?",
+				default: false,
+			},
+		])
+		.then((data) => {
+			// data for employee types
 
-function init()
-{
-    console.log("Please Build your Team.")
-    addManager().then(
-        addTeamMember
-    );
-  
-   
+			let { name, id, email, github, school, confirmAddNew } = data;
+			let employee;
+			console.log(askRole.role);
+			if (askRole.role === "Engineer") {
+				employee = new Engineer(name, id, email, github);
+
+				// console.log(employee);
+			} else if (askRole.role === "Intern") {
+				employee = new Intern(name, id, email, school);
+
+				console.log(employee);
+			}
+
+			team.push(employee);
+			console.log(team);
+			console.log(outputPath);
+			if (confirmAddNew) {
+				return addTeamMember(team);
+			} else {
+				return team;
+			}
+		});
+}
+writeFile = (data) => {
+    if (!fs.existsSync(OUTPUT_DIR)){
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+
+	fs.writeFile(outputPath, data, (err) => {
+		if (err) {
+			console.log(err);
+			return;
+		} else {
+			console.log("Profile created");
+		}
+	});
+};
+
+function init() {
+	console.log("Please Build your Team.");
+	addManager()
+		.then(addTeamMember)
+		.then((team) => {
+			return render(team);
+		})
+		.then((data) => {
+			console.log(outputPath);
+			return writeFile(data);
+		});
 }
 
 init();
