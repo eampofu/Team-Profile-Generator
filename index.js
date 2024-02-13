58c613f41ada480ddd32a358a833bdd56c1d03e1
Eddington <eampofu@gmail.com>
2024-02-13 18:14:26 +0200

    added code to ask questions about the manager and removed the double assignment of properrties in the Engineer and Intern classes

--- index.js
+++ index.js
@@ -13,3 +13,51 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const team=[];
+
+const addManager = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the team manager's name?", 
+          
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the team manager's ID.",
+          
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the team manager's email.",
+         
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "What is the team manager's office number",
+          
+        }
+    ])
+    .then(data => {
+        const  { name, id, email, officeNumber } = data; 
+        const manager = new Manager (name, id, email, officeNumber);
+
+        team.push(manager); 
+        console.log(manager); 
+    })
+};
+
+
+function init()
+{
+    console.log("Please Build your Team.")
+    addManager();
+  
+   
+}
+
+init();
